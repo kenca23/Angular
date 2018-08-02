8d82e7f224e347514346503e0da4c0c731af255f
Kenneth Calvo <kenca23@gmail.com>
2018-08-02 17:14:06 -0600

    cmp-databinding exersises

---
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreaded') blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent: string}>();
+  newServerName = '';
+  newServerContent = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddServer() {
+    this.serverCreated.emit({
+      serverName: this.newServerContent,
+      serverContent: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.blueprintCreated.emit({
+      blueprintName: this.newServerContent,
+      blueprintContent: this.newServerContent
+    });
+  }
+
+}
